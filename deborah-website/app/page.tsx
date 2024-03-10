be93aecc85eec2fc42e6c5517edb2ca3b85758f7
Erin Murphy <erin.murphy@atomicobject.com>
2024-03-10 18:11:37 -0400

    First draft - Homepage w office pic, headshot, bio

--- deborah-website/app/page.tsx
+++ deborah-website/app/page.tsx
@@ -1,113 +1,74 @@
+import React from "react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col bg-background">
+      <div className="relative" style={{ height: "95vh" }}>
+        <Image
+          src="/images/office.jpeg"
+          alt="Deborah's office"
+          layout="fill"
+          objectFit="cover"
+          className="brightness-50"
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center">
+          <h1 className="text-white text-5xl text-center p-4">
+            Deborah Berman, LCSW
+          </h1>
+          <p className="text-white text-2xl text-center ">
+            Therapy + Counseling Services
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="p-10">
+        <h2 className="mb-4 text-3xl">About Deborah</h2>
+        <div className="flex flex-col md:flex-row gap-8 md:items-start items-center">
+          <div className="relative shrink-0 w-1/2 h-80 md:w-1/5 md:h-60">
+            <Image
+              src="/images/deborah.jpeg"
+              alt="A headshot of Deborah smiling"
+              layout="fill"
+              objectFit="cover" // Adjust as needed to control how the image fits within its container
+              className="rounded border-2" // Optional: Adds rounded corners to the image
+            />
+          </div>
+          <div className="flex-grow">
+            <p className="mb-4">
+              The work of therapy starts even before the first session is
+              scheduled. Who will hear you? Who will understand your unique way
+              of sharing your concerns, goals, confusion, and values? Extensive
+              experience has deepened my knowledge of the human experience and
+              enriched what I have to offer. In sessions, I will listen as you
+              tell your story to understand and to help you make the next move
+              in your life feeling more fulfilled and at peace.
+            </p>
+            <p className="mb-4">
+              I am a graduate of the Smith School for Social Work ('92) where
+              the majority of training was in the field under the supervision of
+              seasoned professionals. Since graduation, I have trained in
+              mindfulness, cognitive therapy, and family systems as well as many
+              other topics. I also have a special interest in learning
+              disabilities and how they affect children and families. Please see
+              other areas of the website where techniques are further explained.
+            </p>
+            <p className="mb-4">
+              My life outside of the office enriches my life within it. I am an
+              avid gardener, I enjoy cooking, singing, and tai-chi. My
+              undergraduate major was in Art History and I have maintained an
+              interest in the arts. These interests bring me to the consulting
+              room with a freshness and openness to you as a patient. While
+              there are many techniques and schools of therapy, the foundation
+              of the process is connection, which I hope to offer you in our
+              work together. I encourage you to take that first step and
+              schedule a consultation free of charge.
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="p-10">
+        <h2 className="mb-4 text-3xl">Schedule</h2>
       </div>
-    </main>
+    </div>
   );
 }
