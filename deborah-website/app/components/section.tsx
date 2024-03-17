fe499cf11b59bc12d4426f88b8f71eb2b40eff1d
Erin Murphy <erin.murphy@atomicobject.com>
2024-03-17 16:44:30 -0400

    Create Section component (+ merge conflicts)

--- deborah-website/app/components/section.tsx
+++ deborah-website/app/components/section.tsx
@@ -8,24 +8,28 @@ type Props = {
 };
 
 const Section: React.FC<Props> = ({ imgSrc = "", altText = "", children }) => {
-  return <>
-    {imgSrc ? (
-      <div className="relative" style={{ height: "95vh" }}>
-        <Image
-          src={imgSrc}
-          alt={altText}
-          className="brightness-50"
+  return (
+    <>
+      {imgSrc ? (
+        <div className="relative" style={{ height: "95vh" }}>
+          <Image
+            src={imgSrc}
+            alt={altText}
           fill
           sizes="100vw"
+            className="brightness-50 bg-cover bg-center h-64 w-full"
           style={{
             objectFit: "cover"
           }} />
-        {children}
-      </div>
-    ) : (
-      <div className="p-10">{children}</div>
-    )}
-  </>;
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center">
+            {children}
+          </div>
+        </div>
+      ) : (
+        <div className="p-10">{children}</div>
+      )}
+    </>
+  );
 };
 
 export default Section;
